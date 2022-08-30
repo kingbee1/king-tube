367638d714e1952e7bc858e513127a167e3b19d0
Abiodun <abiodunhodonu@gmail.com>
2022-08-30 23:00:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/system';
+
+const App = () =>  (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      Navbarrrr
+    </Box>
+  </BrowserRouter>
+)
+
+
+export default App
