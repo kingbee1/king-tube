879ef750d5ebb961b49acfd2f1c0cc7ee3207de1
Abiodun <abiodunhodonu@gmail.com>
2022-09-08 22:36:52 +0100

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import { Stack } from "@mui/system"
 import { Link } from "react-router-dom"
+import SearchBar from "./SearchBar"
 
 import { logo } from "../utils/constants"
 
 const Navbar = () => {
   return (
-    <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: '0', justiftContent: 'space-between'}}>
-      Navbar
+    <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: '0', justifyContent: 'space-between'}}>
+      <Link to='/' style={{display: 'flex', alignItems: 'center'}}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
     </Stack>
   )
 }
